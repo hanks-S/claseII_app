02b92e6cc73521ddb2c3b73eae36365837d3e656
hanks-S <michaelchilito@unicauca.edu.co>
2024-11-01 16:49:12 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost/crud'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://appii_postgresql_user:WveKHbwk4H3D9dWSpFFGG2lcONuhNyFB@dpg-csijm5d2ng1s739kc380-a.oregon-postgres.render.com/appii_postgresql'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
