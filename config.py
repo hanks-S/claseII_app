7579e32794cdee8bf23c0511bfa0c14b3d9b8dff
hans-S <michaelchilito@unicauca.edu.co>
2024-11-01 17:10:25 -0500

    Subiendo app con docker

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://testdb_fwms_user:MclrHbdtGttpwx7NvXqN286mtg4LfEgD@dpg-crue83jv2p9s73epvrf0-a/testdb_fwms'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://appii_postgresql_user:WveKHbwk4H3D9dWSpFFGG2lcONuhNyFB@dpg-csijm5d2ng1s739kc380-a.oregon-postgres.render.com/appii_postgresql'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
